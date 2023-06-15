f8627a16e1c3746956d4df932605e1f65b485f54
giangnguyenbkhn <nguyengiang20112001@gmail.com>
2023-06-15 11:49:46 +0700

    15/6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+// import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<Provider store={store} >
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
